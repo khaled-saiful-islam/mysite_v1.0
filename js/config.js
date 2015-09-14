14201e225e4133fd5ed3acfd34a9a69e039580d8
Khaled Saiful Islam <saiful.islam@bjitgroup.com>
2015-09-13 22:55:38 -0700

    Initial Commit

---
+++ js/config.js
@@ -0,0 +1,79 @@
+/* Google MAP Settings*/
+var lat= 100.73176;  //Change the value with your address Latitude
+var lng= 50.40640;  //Change the value with your address Longitude
+  
+//Skill Chart Data
+var skillData = {
+    skill1: [
+    {
+        title: "CakePHP 90%",
+        color: "#1b7ee4",
+        border: "4px solid #8cc2f9",
+        value: '90%'
+    },
+    {
+        title: "HTML/CSS 85%",
+        color: "#1b7ee4",
+        border: "4px solid #8cc2f9",
+        value: '85%'
+    },
+    {
+        title: "PHP & MySQL 80%",
+        color: "#1b7ee4",
+        border: "4px solid #8cc2f9",
+        value: '80%'
+    },
+    {
+        title: "Wordpress 75%",
+        color: "#1b7ee4",
+        border: "4px solid #8cc2f9",
+        value: '70%'
+    },
+    {
+        title: "JS/Jquery 70%",
+        color: "#1b7ee4",
+        border: "4px solid #8cc2f9",
+        value: '60%'
+    }		
+    ],
+    skill2: [
+    {
+        title: "Adobe&nbsp;CS 70%",
+        color: "#f3377a",
+        border: "4px solid #f4709f",
+        value: '70%'
+    }
+    ],
+    skill3: [
+    {
+        title: "Google API 70%",
+        color: "#87b615",
+        border: "4px solid #e4f9b1",
+        value: '70%'
+    },
+    {
+        title: "Linkedin API 60%",
+        color: "#87b615",
+        border: "4px solid #e4f9b1",
+        value: '70%'
+    },
+    {
+        title: "Payment Gateway 70%",
+        color: "#87b615",
+        border: "4px solid #e4f9b1",
+        value: '70'
+    },
+    {
+        title: "Json & Ajax 65%",
+        color: "#87b615",
+        border: "4px solid #e4f9b1",
+        value: '65%'
+    },
+    {
+        title: "XML 70%",
+        color: "#87b615",
+        border: "4px solid #e4f9b1",
+        value: '70%'
+    }
+    ]
+}
